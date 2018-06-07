15bf84676955cfd6fa9daf521da2298c3566f879
Daniel Restrepo Mejia <drestrepom@pslcol.com.co>
2018-06-07 14:11:22 -0500

    trying to fix the mess

--- test/BuyTshirt.spec.ts
+++ test/BuyTshirt.spec.ts
@@ -14,45 +14,59 @@ import {
 } from '../src/page';
 
 
-describe('Buy a t-shirt', () => {
-  const menuContentPage: MenuContentPage = new MenuContentPage();
-  const productDetailPage: ProductDetailPage = new ProductDetailPage();
-  const productListPage: ProductListPage = new ProductListPage();
-  const productAdded: ProductAddedModalPage = new ProductAddedModalPage();
-  const summaryStepPage: SummaryStepPage = new SummaryStepPage();
-  const signInStepPage: SignInStepPage = new SignInStepPage();
-  const addressStepPage: AddressStepPage = new AddressStepPage();
-  const shippingStepPage: ShippingStepPage = new ShippingStepPage();
-  const paymentStepPage: PaymentStepPage = new PaymentStepPage();
-  const orderResumePage: OrderResumePage = new OrderResumePage();
-  const bankPaymentPage: BankPaymentPage = new BankPaymentPage();
-  const username: string = 'aperdomobo@gmail.com';
-  const password: string = 'WorkshopProtractor';
+describe('In a shopping webpage', () => {
+  const webpage: string = 'http://automationpractice.com/';
 
-  it('then should be bought a t-shirt', async () => {
-    await browser.get('http://automationpractice.com/');
-    
-    await menuContentPage.goToTShirtMenu();
+  beforeAll(async () => {
+    await browser.get(webpage);
+  });
 
-    await productDetailPage.goToProductDetail(); 
+  describe('I want to buy a t-shirt', () => {
+    const menuContentPage: MenuContentPage = new MenuContentPage();
+    const productDetailPage: ProductDetailPage = new ProductDetailPage();
+    const productListPage: ProductListPage = new ProductListPage();
+    const productAdded: ProductAddedModalPage = new ProductAddedModalPage();
+    const summaryStepPage: SummaryStepPage = new SummaryStepPage();
 
-    await productListPage.addProductToCart();
+    beforeAll(async () => {
+      await menuContentPage.goToTShirtMenu();
+      await productDetailPage.goToProductDetail();
+      await productListPage.addProductToCart();
+      await productAdded.checkoutProduct();
+      await summaryStepPage.confirmShoppingCartSummary();
+    });
 
-    await productAdded.checkoutProduct();
+    describe('and login to the webpage', () => {
+      const signInStepPage: SignInStepPage = new SignInStepPage();
+      const username: string = 'aperdomobo@gmail.com';
+      const password: string = 'WorkshopProtractor';  
+      beforeAll(async () => {
+        await signInStepPage.signInToAccount(username, password);
+      });
 
-    await summaryStepPage.confirmShoppingCartSummary();
+      describe('select the address', () => {
+        const addressStepPage: AddressStepPage = new AddressStepPage();
+        const shippingStepPage: ShippingStepPage = new ShippingStepPage();
+        beforeAll(async () => {
+          await addressStepPage.checkoutAddress();
+          await shippingStepPage.shippingCheckout();
+        });
 
-    await signInStepPage.signInToAccount(username, password);
+        describe('And make the bank payment', () => {
+          const paymentStepPage: PaymentStepPage = new PaymentStepPage();
+          const orderResumePage: OrderResumePage = new OrderResumePage();
+          const bankPaymentPage: BankPaymentPage = new BankPaymentPage();
+          beforeAll(async () => {
+            await paymentStepPage.selectPaymenMethod();
+            await bankPaymentPage.confirmPayment();
+          });
 
-    await addressStepPage.checkoutAddress();
-
-    await shippingStepPage.shippingCheckout();
-
-    await paymentStepPage.selectPaymenMethod();
-
-    await bankPaymentPage.confirmPayment();
-
-    await expect(orderResumePage.confirmOrder())
-      .toBe('Your order on My Store is complete.');
+          it('then should be bought a t-shirt', async () => {
+            await expect(orderResumePage.confirmOrder())
+              .toBe('Your order on My Store is complete.');
+          });
+        });
+      });
+    });
   });
 });
