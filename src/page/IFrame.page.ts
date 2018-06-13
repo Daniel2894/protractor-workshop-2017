e8467d3f8cd568c220e1b984ccb3e0a9fc3d1ade
Daniel Restrepo Mejia <drestrepom@pslcol.com.co>
2018-06-13 18:37:15 -0500

    working with frames

--- src/page/IFrame.page.ts
+++ src/page/IFrame.page.ts
@@ -6,12 +6,28 @@ export class IFramePage {
     return browser.executeScript(`document.getElementById("IF1").setAttribute("height", "${newHeight}")`)
   }
 
-  public get frameElement(): ElementFinder {
+  private get frameElement(): ElementFinder {
     return element(by.id('IF1'));
+  }
+
+  private get getMainTitle(): ElementFinder {
+    return element(by.css('#content h1'));
   }
 
   public async getHeight(): Promise<number> {
     const height = await this.frameElement.getAttribute('height');
     return Number(height);
+  }
+
+  public mainTitle(): promise.Promise<string> {
+    return this.getMainTitle.getText();
+  }
+
+  public switchFrame(): promise.Promise<void> {
+    return browser.switchTo().frame(this.frameElement.getWebElement());
+  }
+
+  public switchMainFrame(): promise.Promise<void> {
+    return browser.switchTo().defaultContent();
   }
 }
--- test/IFrame.spec.ts
+++ test/IFrame.spec.ts
@@ -12,6 +12,8 @@ describe('If i want to modify an IFrame', () => {
 
   describe('I want to modify the height of the IFrame', () => {
     const height: number = 400;
+    const expectedTitle: string = 'Sample Iframe page';
+    const frameSwitchedTitle: string = 'Practice Automation Form';
 
     beforeAll(async () => {
       await iFramePage.setHeight(height);
@@ -20,6 +22,31 @@ describe('If i want to modify an IFrame', () => {
     it('Should have the new height', async () => {
       await expect(iFramePage.getHeight())
         .toBe(height);
-    });    
+    });
+
+    it('Should have "Sample IFrame page" for title', async () =>{
+      await expect(iFramePage.mainTitle())
+        .toBe(expectedTitle);
+    });
+
+    describe('And should switch to Frame 1', () => {
+      beforeAll(async () => {
+        await iFramePage.switchFrame();
+      });
+
+      it('Should have the title "Practice Automation Form"', async () => {
+        await expect(iFramePage.mainTitle()).toBe(frameSwitchedTitle);
+      });
+
+      describe('And return to the main frame', () => {
+        beforeAll(async () => {
+          await iFramePage.switchMainFrame();
+        });
+
+        it('Should have the title of the main frame: "Sample Iframe page"', async () => {
+          await expect(iFramePage.mainTitle()).toBe(expectedTitle);
+        });
+      });
+    });
   });
 });
