c71f53d2129cca33bd2ed2e21ecf43b1c3c14f15
Daniel Restrepo Mejía <daniel2894@gmail.com>
2018-06-13 17:54:35 -0500

    working with iframes (#20)
    * working with iframes
    
    * valor quemado eliminado
    
    * change of let for const

---
+++ src/page/IFrame.page.ts
@@ -0,0 +1,17 @@
+import { element, by, ElementFinder, browser, promise } from 'protractor';
+
+export class IFramePage {
+ 
+  public setHeight(newHeight: number): promise.Promise<void> {
+    return browser.executeScript(`document.getElementById("IF1").setAttribute("height", "${newHeight}")`)
+  }
+
+  public get frameElement(): ElementFinder {
+    return element(by.id('IF1'));
+  }
+
+  public async getHeight(): Promise<number> {
+    const height = await this.frameElement.getAttribute('height');
+    return Number(height);
+  }
+}
