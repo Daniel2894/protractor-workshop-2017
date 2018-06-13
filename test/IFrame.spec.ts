ccffb9bb31dc157ba7a04ad7728393b8a7f812f5
Daniel Restrepo Mejia <drestrepom@pslcol.com.co>
2018-06-13 16:01:38 -0500

    working with iframes

---
+++ test/IFrame.spec.ts
@@ -0,0 +1,25 @@
+import { browser } from 'protractor';
+import { IFramePage } from '../src/page';
+
+
+describe('If i want to modify an IFrame', () => {
+  const webpage: string = 'http://toolsqa.com/iframe-practice-page/';
+  const iFramePage: IFramePage = new IFramePage();  
+  
+  beforeAll(async () => {
+    await browser.get(webpage);
+  })
+
+  describe('I want to modify the height of the IFrame', () => {
+    const height: number = 400;
+
+    beforeAll(async () => {
+      await iFramePage.setHeight(height);
+    })
+
+    it('Should have the new height', async () => {
+      await expect(iFramePage.getHeight())
+        .toBe(400);
+    });    
+  });
+});
