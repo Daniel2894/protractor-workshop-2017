501f18ea3fca79d3737141b871d8c787d7c38483
Daniel Restrepo Mejia <drestrepom@pslcol.com.co>
2018-06-13 09:37:04 -0500

    more locators

---
+++ test/Locators.spec.ts
@@ -0,0 +1,38 @@
+import { browser } from 'protractor';
+import { PersonalInformationPage } from '../src/page'
+
+describe('In the Tools QA webpage', () => {
+  const webpage = 'http://toolsqa.com/automation-practice-form/';
+
+  beforeAll(async () => {
+    await browser.get(webpage);
+  });
+
+  describe('I want to fill the form', () => {
+    const personalInformationPage: PersonalInformationPage = new PersonalInformationPage();
+
+    beforeAll(async () => {
+      await personalInformationPage.fillForm({
+        firstName: 'Alejandro',
+        lastName: 'Perdomo',
+        sex: 'Male',
+        experience: 7,
+        profession: ['Automation Tester'],
+        tools: ['Selenium Webdriver'],
+        continent: 'South America',
+        commands: [
+          'Browser Commands',
+          'Navigation Commands',
+          'Switch Commands',
+          'Wait Commands',
+          'WebElement Commands']
+      });
+    });
+
+    describe('and get the form title', () => {
+      it('Should have the title', async () => {
+        expect(await personalInformationPage.formTitle()).toBe('Practice Automation Form');
+      })
+    })    
+  });
+})
