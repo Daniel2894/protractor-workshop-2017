2c9fc898173e97f71976b93fc82f1981d3505740
Daniel Restrepo Mejía <daniel2894@gmail.com>
2018-06-14 10:56:27 -0500

    working with frames (#21)
    * working with frames
    
    * name of functions

--- src/page/IFrame.page.ts
+++ src/page/IFrame.page.ts
@@ -6,12 +6,28 @@ export class IFramePage {
     return browser.executeScript(`document.getElementById("IF1").setAttribute("height", "${newHeight}")`)
   }
 
-  public get frameElement(): ElementFinder {
+  private get frameElement(): ElementFinder {
     return element(by.id('IF1'));
+  }
+
+  private get title(): ElementFinder {
+    return element(by.css('#content h1'));
   }
 
   public async getHeight(): Promise<number> {
     const height = await this.frameElement.getAttribute('height');
     return Number(height);
+  }
+
+  public getTitle(): promise.Promise<string> {
+    return this.title.getText();
+  }
+
+  public switchToFrame(): promise.Promise<void> {
+    return browser.switchTo().frame(this.frameElement.getWebElement());
+  }
+
+  public switchToMainFrame(): promise.Promise<void> {
+    return browser.switchTo().defaultContent();
   }
 }
