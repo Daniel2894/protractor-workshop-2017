7a7da5e518f20ea5872e953420d71e4a1a0a6aa1
Daniel Restrepo Mejía <daniel2894@gmail.com>
2018-06-14 15:39:10 -0500

    uploaded file (#22)
    * uploaded file
    
    * change
    
    * split and json modified
    
    * config

--- test/Locators.spec.ts
+++ test/Locators.spec.ts
@@ -3,6 +3,23 @@ import { PersonalInformationPage } from '../src/page'
 
 describe('In the Tools QA webpage', () => {
   const webpage = 'http://toolsqa.com/automation-practice-form/';
+  const formInformation = {
+    firstName: 'Alejandro',
+    lastName: 'Perdomo',
+    sex: 'Male',
+    experience: 7,
+    profession: ['Automation Tester'],
+    tools: ['Selenium Webdriver'],
+    continent: 'South America',
+    file: './resources/image.jpg',
+    filename: 'image.jpg',
+    commands: [
+      'Browser Commands',
+      'Navigation Commands',
+      'Switch Commands',
+      'Wait Commands',
+      'WebElement Commands']
+  };
 
   beforeAll(async () => {
     await browser.get(webpage);
@@ -12,26 +29,18 @@ describe('In the Tools QA webpage', () => {
     const personalInformationPage: PersonalInformationPage = new PersonalInformationPage();
 
     beforeAll(async () => {
-      await personalInformationPage.fillForm({
-        firstName: 'Alejandro',
-        lastName: 'Perdomo',
-        sex: 'Male',
-        experience: 7,
-        profession: ['Automation Tester'],
-        tools: ['Selenium Webdriver'],
-        continent: 'South America',
-        commands: [
-          'Browser Commands',
-          'Navigation Commands',
-          'Switch Commands',
-          'Wait Commands',
-          'WebElement Commands']
-      });
+      await personalInformationPage.fillForm(formInformation);
     });
 
     describe('and get the form title', () => {
       it('Should have the title', async () => {
         expect(await personalInformationPage.formTitle()).toBe('Practice Automation Form');
+      });
+
+      describe('Then should upload an image', () => {
+        it('Should exist image.jpg in the webpage', async () => {
+          expect(await personalInformationPage.getUploadedImage()).toBe(formInformation.filename);
+        });
       });
     });
   });
